c7b6dfa566a127bf7828eb4a9f11c21bc10ba873
Mariana Mattana <mariana.dev2017@gmail.com>
2018-01-29 11:22:30 -0200

    added pundit

---
+++ app/policies/game_policy.rb
@@ -0,0 +1,41 @@
+class GamePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      Game.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    confirm_user
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    confirm_user
+  end
+
+  private
+
+  def confirm_user
+    @record.user == @user
+  end
+end
